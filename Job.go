f2157e92a2913dfeaa6369bbc8b2127ff30d2022
Aldren Terante <aldrenterante@altus-dc.com>
2018-06-28 18:14:00 +0800

    do clean up with go fmt

--- Job.go
+++ Job.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-  "log"
-  "time"
-  "errors"
-  "database/sql"
+	"database/sql"
+	"errors"
+	"log"
+	"time"
 
-  _ "github.com/go-sql-driver/mysql"
-  "github.com/go-ozzo/ozzo-validation"
-  // "github.com/go-ozzo/ozzo-validation/is"
+	"github.com/go-ozzo/ozzo-validation"
+	_ "github.com/go-sql-driver/mysql"
+	// "github.com/go-ozzo/ozzo-validation/is"
 )
 
 type Job struct {
-  Id int `json:"id"`
-  UserId int `json:"user_id"`
-  Post string `json:"post"`
-  Location string `json:"location"`
-  Company string `json:"company"`
-  CreatedDate time.Time `json:"created_date,string"`
-  UpdatedDate time.Time `json:"updated_date,string"`
+	Id          int       `json:"id"`
+	UserId      int       `json:"user_id"`
+	Post        string    `json:"post"`
+	Location    string    `json:"location"`
+	Company     string    `json:"company"`
+	CreatedDate time.Time `json:"created_date,string"`
+	UpdatedDate time.Time `json:"updated_date,string"`
 }
 
 func (j Job) validate() error {
-  return validation.ValidateStruct(&j,
-    validation.Field(&j.Post, validation.Required),
-    validation.Field(&j.Location, validation.Required),
-    validation.Field(&j.Company, validation.Required),
-  )
+	return validation.ValidateStruct(&j,
+		validation.Field(&j.Post, validation.Required),
+		validation.Field(&j.Location, validation.Required),
+		validation.Field(&j.Company, validation.Required),
+	)
 }
 
 func (j *Job) getJob(db *sql.DB) (map[string]interface{}, error) {
-  rows, err := db.Query(`
+	rows, err := db.Query(`
     SELECT
       jobs.id,
       jobs.post,
@@ -47,56 +47,56 @@ func (j *Job) getJob(db *sql.DB) (map[string]interface{}, error) {
     WHERE
       jobs.id = ?
   `, j.Id)
-  if err != nil {
-    log.Fatal(err)
-    return nil, err
-  }
-  defer rows.Close()
-  var u User
-  for rows.Next() {
-    rows.Scan(&j.Id, &j.Post, &j.Location, &j.Company, &j.CreatedDate, &j.UpdatedDate, &u.Name)
-    return map[string]interface{}{
-      "id" : j.Id,
-      "post": j.Post,
-      "location": j.Location,
-      "company": j.Company,
-      "created_date": j.CreatedDate,
-      "updated_date": j.UpdatedDate,
-      "name": u.Name,
-    }, nil
-  }
-  return nil, errors.New("No job post found.")
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer rows.Close()
+	var u User
+	for rows.Next() {
+		rows.Scan(&j.Id, &j.Post, &j.Location, &j.Company, &j.CreatedDate, &j.UpdatedDate, &u.Name)
+		return map[string]interface{}{
+			"id":           j.Id,
+			"post":         j.Post,
+			"location":     j.Location,
+			"company":      j.Company,
+			"created_date": j.CreatedDate,
+			"updated_date": j.UpdatedDate,
+			"name":         u.Name,
+		}, nil
+	}
+	return nil, errors.New("No job post found.")
 
-  err = rows.Err()
-  if err != nil {
-    log.Fatal(err)
-    return map[string]interface{}{}, err;
-  }
-  return map[string]interface{}{}, nil
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+		return map[string]interface{}{}, err
+	}
+	return map[string]interface{}{}, nil
 }
 
 func (j *Job) createJob(db *sql.DB, userId int) error {
-  query := `
+	query := `
     INSERT INTO jobs (user_id, post, location, company)
     VALUES (?, ?, ?, ?)
   `
-  var err error
-  _, err = db.Exec(query, userId, j.Post, j.Location, j.Company)
-  if err != nil {
-    return err
-  }
+	var err error
+	_, err = db.Exec(query, userId, j.Post, j.Location, j.Company)
+	if err != nil {
+		return err
+	}
 
-  err = db.QueryRow("SELECT LAST_INSERT_ID()").Scan(&j.Id)
+	err = db.QueryRow("SELECT LAST_INSERT_ID()").Scan(&j.Id)
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
 func (j *Job) getJobs(db *sql.DB, offset, limit int) ([]map[string]interface{}, error) {
-  rows, err := db.Query(`
+	rows, err := db.Query(`
     SELECT
       jobs.id,
       jobs.post,
@@ -113,32 +113,32 @@ func (j *Job) getJobs(db *sql.DB, offset, limit int) ([]map[string]interface{}, 
     LIMIT ?
     OFFSET ?
   `, limit, offset)
-  if err != nil {
-    return nil, err
-  }
-  jobs := []Job{}
-  allJobs := make([]map[string]interface{}, len(jobs))
-  for rows.Next() {
-    var j Job
-    var u User
-    if err := rows.Scan(&j.Id, &j.Post, &j.Location, &j.Company, &j.CreatedDate, &j.UpdatedDate, &u.Name); err != nil {
-      return nil, err
-    }
-    allJobs = append(allJobs, map[string]interface{}{
-      "id" : j.Id,
-      "post": j.Post,
-      "location": j.Location,
-      "company": j.Company,
-      "created_date": j.CreatedDate,
-      "updated_date": j.UpdatedDate,
-      "name": u.Name,
-    })
-  }
-  return allJobs, nil
+	if err != nil {
+		return nil, err
+	}
+	jobs := []Job{}
+	allJobs := make([]map[string]interface{}, len(jobs))
+	for rows.Next() {
+		var j Job
+		var u User
+		if err := rows.Scan(&j.Id, &j.Post, &j.Location, &j.Company, &j.CreatedDate, &j.UpdatedDate, &u.Name); err != nil {
+			return nil, err
+		}
+		allJobs = append(allJobs, map[string]interface{}{
+			"id":           j.Id,
+			"post":         j.Post,
+			"location":     j.Location,
+			"company":      j.Company,
+			"created_date": j.CreatedDate,
+			"updated_date": j.UpdatedDate,
+			"name":         u.Name,
+		})
+	}
+	return allJobs, nil
 }
 
 func (j *Job) updateJob(db *sql.DB, userId int, jobId int) (map[string]interface{}, error) {
-  stmt, err := db.Prepare(`
+	stmt, err := db.Prepare(`
     UPDATE
       jobs
     SET
@@ -151,26 +151,26 @@ func (j *Job) updateJob(db *sql.DB, userId int, jobId int) (map[string]interface
     AND
       user_id = ?
   `)
-  res, err := stmt.Exec(j.Post, j.Location, j.Company, jobId, userId)
-  if err != nil {
-    log.Fatal(err)
-    return nil, err
-  }
+	res, err := stmt.Exec(j.Post, j.Location, j.Company, jobId, userId)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
 
-  count, err := res.RowsAffected()
-  if err != nil {
-    log.Fatal(err)
-  }
+	count, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  if count != 0 {
-    return map[string]interface{}{}, nil
-  }
+	if count != 0 {
+		return map[string]interface{}{}, nil
+	}
 
-  return nil, errors.New("No job post found.")
+	return nil, errors.New("No job post found.")
 }
 
 func (j *Job) deleteJob(db *sql.DB, userId int, jobId int) (map[string]interface{}, error) {
-  stmt, err := db.Prepare(`
+	stmt, err := db.Prepare(`
     DELETE FROM
       jobs
     WHERE
@@ -178,20 +178,20 @@ func (j *Job) deleteJob(db *sql.DB, userId int, jobId int) (map[string]interface
     AND
       user_id = ?
   `)
-  res, err := stmt.Exec(jobId, userId)
-  if err != nil {
-    log.Fatal(err)
-    return nil, err
-  }
+	res, err := stmt.Exec(jobId, userId)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
 
-  count, err := res.RowsAffected()
-  if err != nil {
-    log.Fatal(err)
-  }
+	count, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  if count != 0 {
-    return map[string]interface{}{}, nil
-  }
+	if count != 0 {
+		return map[string]interface{}{}, nil
+	}
 
-  return nil, errors.New("No job post found.")
+	return nil, errors.New("No job post found.")
 }
